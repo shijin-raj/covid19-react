0ca85cb0449ba4ba181a6d776965520c1c9a722e
Shijin Raj <shijin802@gmail.com>
2021-10-17 11:15:28 +0530

    Added click to select functionality

--- src/components/sidebar/SideBar.js
+++ src/components/sidebar/SideBar.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import "./SideBar.css";
 export default function SideBar(props) {
-  console.log(props.states);
   const [stateList, setStateList] = useState(props.states);
 
   useEffect(() => {
@@ -15,7 +14,7 @@ export default function SideBar(props) {
     if (stateList.length > 1) {
       stateList.forEach((element) => {
         states.push(
-          <StateNameListItem name={element.state_name} active={false}></StateNameListItem>
+          <StateNameListItem name={element.state_name} code={element.state_code} key={element.state_code} active={false} onselect={props.onselect}></StateNameListItem>
         );
       });
     }
@@ -29,14 +28,15 @@ function StateNameListItem(props) {
     const [active, setActive] = useState(props.active);
 const toggleActive=()=>{
     setActive(!active);
+    props.onselect(props.code);
 }
 
   return (
       <div className={active?"state-name-item active":"state-name-item"} onClick={toggleActive}>
       {props.name}
       <span>
-      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-right" viewBox="0 0 16 16">
-  <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"/>
+      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-chevron-right" viewBox="0 0 16 16">
+  <path fillRule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"/>
 </svg>
 </span>
       </div>
